efb13a4b52ffe3384d6f4dacf3206d453200aab7
boyone <boyone.g@gmail.com>
2024-07-16 11:31:42 +0700

    Implement first testcase by make it pass

--- unit-test-js/__tests__/point.test.js
+++ unit-test-js/__tests__/point.test.js
@@ -1,3 +1,5 @@
+import calculatePoint from '../src/point';
+
 it('ถ้าราคาสินค้าที่สั่งซื้อเท่ากันกับ 100.00 บาท จำนวนแต้มสะสมที่จะได้รับ ต้องเท่ากันกับ 1 แต้ม', () => {
   // Arrange
   const expectedPoint = 1;
---
+++ unit-test-js/src/point.js
@@ -0,0 +1,3 @@
+export default calculatePoint = (productPrice) => {
+    return 1;
+}
