f8a278a5c3385ba92a532440e812db4f819220d4
boyone <boyone.g@gmail.com>
2024-07-16 11:55:21 +0700

    Refactor calculatePoint

--- unit-test-js/src/point.js
+++ unit-test-js/src/point.js
@@ -1,9 +1,3 @@
 export default calculatePoint = (productPrice) => {
-    if (productPrice === 5269.00) {
-        return Math.floor(productPrice/100);
-    }
-    if (productPrice === 100.00) {
-        return 1;
-    }
-    return 0;
+    return Math.floor(productPrice/100);
 }
