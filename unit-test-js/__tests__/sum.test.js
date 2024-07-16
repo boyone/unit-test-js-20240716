7a2c43f297985c7aaabb9886c5b24b7b18706dce
boyone <boyone.g@gmail.com>
2024-07-16 13:39:21 +0700

    Introduce test table with sum function

--- unit-test-js/__tests__/sum.test.js
+++ unit-test-js/__tests__/sum.test.js
@@ -3,3 +3,14 @@ import sum from '../src/sum';
 it('[0001] should adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+it.each([
+  { name: '[xx001]', left: 1, right: 2, expectedValue: 3 },
+  { name: '[xx002]', left: 1, right: 3, expectedValue: 4 },
+  { name: '[xx003]', left: 1, right: 4, expectedValue: 5 },
+])(
+  '$name should adds $left + $right to equal $expectedValue',
+  ({ left, right, expectedValue }) => {
+    expect(sum(left, right)).toEqual(expectedValue);
+  }
+);
